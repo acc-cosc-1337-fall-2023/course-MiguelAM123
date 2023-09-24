3b81ee647fd32e86e675cac9eaedf2ad2ad7d1c9
MiguelAM123 <143143323+MiguelAM123@users.noreply.github.com>
2023-09-24 01:12:23 +0000

    Homework #4

--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,8 +1,71 @@
 //write include statements
+#include <iostream>
+#include "repetition.h"
 
 //write using statements
 
+using std::cin; using std::cout;
+
 int main() 
 {
-	return 0;
+	int num = 0;
+	int choice = 0;
+	int gcdnum = 0;
+	int gcdnum2 = 0;
+	int gcdvalue = 0;
+	int factorialTotal = 0;
+	char input = 'n';
+
+	
+	do
+	{
+
+	cout << "Select an option 1-3" << "\n";
+    cout << "1.) Factorial" << "\n";
+    cout << "2.) Greatest Common Divisor" << "\n";
+    cout << "3.) Exit" << "\n";
+    
+    cin >>  choice;
+
+    
+		if (choice >= 1 && choice <= 3)
+		{
+			if (choice == 1)
+			{
+				cout << "Enter a Number" << "\n";
+				cin >> num; 
+				cout << "The factorial of " << num; 
+
+				factorialTotal = factorial(num);
+
+				cout << " is " << factorialTotal << "\n";
+
+			}
+
+			else if (choice == 2)
+			{
+				cout << "Enter the first number" << "\n";
+				cin >> gcdnum;
+				cout << "Enter the second number" << "\n";
+				cin >> gcdnum2; 
+
+				gcdvalue = gcd(gcdnum, gcdnum2);
+
+				cout << "The greatest common divisor is " << gcdvalue << "\n";
+
+
+
+			}
+
+			else if (choice == 3)
+			{
+				cout << "Are you sure you want to exit?" << "\n";
+				cin >> input; 
+
+			}
+		}
+	
+	
+	} while (input == 'n' || input == 'N' );
+	
 }
--- src/homework/04_repetition/repetition.cpp
+++ src/homework/04_repetition/repetition.cpp
@@ -1,3 +1,39 @@
 //add include statements
+#include "repetition.h"
+
 
 //add function(s) code here
+
+
+int factorial(int num)
+{
+    int total = 1;
+    while (num > 0)
+    {
+        total *= num;
+        num--;
+    }
+    return total;
+}
+
+
+int gcd(int num1, int num2)
+{
+    int placeholder = 0;
+
+    while (num1 != num2) 
+        {
+           if (num1 < num2)
+           {
+                placeholder = num1;
+                num1 = num2; 
+                num2 = placeholder;
+           }
+        
+            num1 = num1 - num2;
+
+        }
+    return num1;
+}
+
+
